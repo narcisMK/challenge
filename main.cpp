5fa15c6ad7feabc16b431d082cc163b45a3346b8
NarcisMK <nargess.mohammadi.k@gamil.com>
2025-10-06 23:11:02 +0330

    fixing process

--- main.cpp
+++ main.cpp
@@ -1,6 +1,60 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include "sharedData.hpp"
 
+constexpr int MAX_COUNT = 10;
+const std::string SHM_NAME = "/nargess_shm";
+const std::string SEM_NAME = "/nargess_sem";
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    sharedData* data = get_share_memory(SHM_NAME,SEM_NAME);
+    if (!data){
+        return 1;
+    }
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        std::print(stderr,"Failed to fork process");
+        free_share_memory(SHM_NAME, data);
+        return 1;
+    }
+
+    if (pid == 0) {
+        while (true) {
+            sem_wait(&data->semaphore);
+            if (data->counter >= MAX_COUNT) {
+                sem_post(&data->semaphore);
+                break;
+            }
+            std::println("[Receiver] Received: {} " ,data->counter);
+            ++data->counter;
+            std::println("[Receiver] Sending: {} " , data->counter);
+            sem_post(&data->semaphore);
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+    } else {
+        std::println("[Initiator] Starting with: {} " ,data->counter);
+
+        while (true) {
+            sem_wait(&data->semaphore);
+
+            if (data->counter >= MAX_COUNT) {
+                sem_post(&data->semaphore);
+                break;
+            }
+
+            std::println("[Initiator]  Received: {} " ,data->counter );
+            ++data->counter;
+            std::println("[Initiator] Sending: {} " , data->counter );
+            sem_post(&data->semaphore);
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+
+        wait(nullptr);
+        free_share_memory(SHM_NAME, data);
+    }
+
+    std::println("Program finished.");
     return 0;
 }
